2b13b060fbeb498aef879b9053aa62fd7ed5f7a9
elanor <elanor.m.moore@gmail.com>
2020-10-20 13:10:06 -0400

    added documentation to Simplifier

--- src/main/java/Individual/Myfolder/Simplifier.java
+++ src/main/java/Individual/Myfolder/Simplifier.java
@@ -3,32 +3,48 @@ package Individual.Myfolder;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * Simplifies file paths by removing unnecessarily nested folders
+ */
 public class Simplifier {
 
 	public void simplify(String folderPath) {
 
+		/**
+		 * The files/folders within the parent specified by folderPath
+		 */
 		File[] list = new File(folderPath).listFiles();
 
+		/**
+		 * The pathnames of files within the parent
+		 */
 		ArrayList<String> filepaths = new ArrayList<String>();
 
+		/**
+		 * The pathnames of folders within the parent
+		 */
 		ArrayList<String> folderlocations = new ArrayList<String>();
 
+		/**
+		 * Populates filepaths and folderlocations with the relevant pathnames
+		 * Recursively calls this method for any subfolders found
+		 */
 		for (File file : list) {
 			if (!file.isHidden()) {
 				if (file.isDirectory()) {
 					String temp = folderPath + File.separator + file.getName();
 					folderlocations.add(temp);
 					simplify(temp);
-
 				} else {
 					String temp2 = file.getName();
 					filepaths.add(temp2);
-
 				}
 			}
-
 		}
 
+		/**
+		 * Deletes subfolders
+		 */
 		for (String a : folderlocations) {
 			File getfile = new File(a);
 			getfile.delete();
@@ -38,6 +54,9 @@ public class Simplifier {
 
 		String upperfolder = folderPath.substring(0, lastindex);
 
+		/**
+		 * Moves files out of subfolders
+		 */
 		for (String b : filepaths) {
 			if (file.isHidden()) {
 				continue;
@@ -46,7 +65,5 @@ public class Simplifier {
 			System.out.println(b + "  " + upperfolder + File.separator + b);
 			file.renameTo(new File(upperfolder + File.separator + b));
 		}
-
 	}
-
 }
