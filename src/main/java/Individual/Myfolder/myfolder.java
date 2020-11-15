6bcdc3565192073703a3e8b1e6ede8f7c6c1d85c
Yuxin Xie <yuxinxie@YuxindeMacBook-Pro.local>
2020-11-15 21:34:53 +0800

    Make this branch print normal report

--- src/main/java/Individual/Myfolder/myfolder.java
+++ src/main/java/Individual/Myfolder/myfolder.java
@@ -45,8 +45,10 @@ public class myfolder {
 		
 		File[] list = new File(folderPath).listFiles();
 		
-       Report report = new Report();
-       report.printpdfreport(list);
+		Report report= new Report();
+		report.printreport(list);
+		
+    
        
        mergefolder merged= new mergefolder();
        
